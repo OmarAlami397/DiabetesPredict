0b623f46b2d6d5832e4441a65c9ac327b6f7a54c
OmarAlami397 <omaralami397@gmail.com>
2025-01-24 18:22:25 -0500

    Update diabetes_querying.sql

--- diabetes_querying.sql
+++ diabetes_querying.sql
@@ -1,3 +1,9 @@
+-- This is the SQL code used to create the datatable and preprocess the dataset.
+-- This was done using the MySQL Workbench
+
+
+-- First I created the table without a primary key to easily load all the data from the original csv file.
+
 CREATE TABLE diabetes_data (
 	Diabetes_binary INT NOT NULL,
     HighBP INT NOT NULL,
@@ -23,13 +29,19 @@ CREATE TABLE diabetes_data (
     Income INT NOT NULL
 );
 
+
+-- Loading the data from the csv file.
+
 LOAD DATA INFILE 'diabetes_binary_5050split_health_indicators_BRFSS2015.csv' INTO TABLE diabetes_data
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
+-- Creating a primary key using auto increment.
+
 ALTER TABLE diabetes_data
 ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
 
+-- Here I dropped the education and income features because they are simply irrelevant demographic data.
 ALTER TABLE diabetes_data
 DROP COLUMN Education,
 DROP COLUMN Income;
